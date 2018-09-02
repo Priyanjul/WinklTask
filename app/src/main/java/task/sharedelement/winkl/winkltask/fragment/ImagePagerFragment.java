f17215bc70ac7466e325a6d5e62a031ba0ddeecd
Priyanjul <priyanjul5302@gmail.com>
2018-09-02 22:20:09 +0530

    Improved

--- app/src/main/java/task/sharedelement/winkl/winkltask/fragment/ImagePagerFragment.java
+++ app/src/main/java/task/sharedelement/winkl/winkltask/fragment/ImagePagerFragment.java
@@ -19,9 +19,6 @@ import task.sharedelement.winkl.winkltask.adapter.ImagePagerAdapter;
 import java.util.List;
 import java.util.Map;
 
-/**
- * A fragment for displaying a pager of images.
- */
 public class ImagePagerFragment extends Fragment {
 
   private ViewPager viewPager;
@@ -32,8 +29,6 @@ public class ImagePagerFragment extends Fragment {
       @Nullable Bundle savedInstanceState) {
     viewPager = (ViewPager) inflater.inflate(R.layout.fragment_pager, container, false);
     viewPager.setAdapter(new ImagePagerAdapter(this));
-    // Set the current position and add a listener that will update the selection coordinator when
-    // paging the images.
     viewPager.setCurrentItem(MainActivity.currentPosition);
     viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
       @Override
@@ -44,7 +39,6 @@ public class ImagePagerFragment extends Fragment {
 
     prepareSharedElementTransition();
 
-    // Avoid a postponeEnterTransition on orientation change, and postpone only of first creation.
     if (savedInstanceState == null) {
       postponeEnterTransition();
     }
@@ -52,9 +46,6 @@ public class ImagePagerFragment extends Fragment {
     return viewPager;
   }
 
-  /**
-   * Prepares the shared element transition from and back to the grid fragment.
-   */
   private void prepareSharedElementTransition() {
     Transition transition =
         TransitionInflater.from(getContext())
@@ -63,23 +54,17 @@ public class ImagePagerFragment extends Fragment {
 
       setSharedElementEnterTransition(transition);
 
-    // A similar mapping is set at the GridFragment with a setExitSharedElementCallback.
     setEnterSharedElementCallback(
         new SharedElementCallback() {
           @Override
           public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
-            // Locate the image view at the primary fragment (the ImageFragment that is currently
-            // visible). To locate the fragment, call instantiateItem with the selection position.
-            // At this stage, the method will simply return the fragment at the position and will
-            // not create a new one.
+
             Fragment currentFragment = (Fragment) viewPager.getAdapter()
                 .instantiateItem(viewPager, MainActivity.currentPosition);
             View view = currentFragment.getView();
             if (view == null) {
               return;
             }
-
-            // Map the first shared element name to the child ImageView.
             sharedElements.put(names.get(0), view.findViewById(R.id.image));
           }
         });
